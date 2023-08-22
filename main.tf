3f19a1d66b6336a0c5c6c81fe5697044ff2faee6
thamilgowtham <135104448+thamilgowtham@users.noreply.github.com>
2023-08-22 14:34:57 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,30 +31,15 @@ resource "aws_security_group" "mysg" {
   description = "New security group"
   vpc_id      = aws_vpc.myvpc.id
   ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
+    description      = "ALL"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
   egress {
+    description      = "ALL"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"

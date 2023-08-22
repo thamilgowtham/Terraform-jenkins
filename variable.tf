064fb8c5acdf9bd9fa6d9646956e66afb5bf9f8a
thamilgowtham <135104448+thamilgowtham@users.noreply.github.com>
2023-08-22 16:27:53 +1000

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,42 @@
+variable "vmname" {
+  default = "Jendoc"
+}
+variable "azone1" {
+  default = "us-west-2a" 
+}
+variable "azone2" {
+  default = "us-west-2b"
+}
+variable "subname1" {
+  default = "Sub01"
+}
+variable "subname2" {
+  default = "Sub02"
+}
+variable "Netif1" {
+  default = "NIF01"
+}
+variable "Netif2" {
+  default = "NIF02"
+}
+variable "subnetid1" {
+  default = "10.0.1.0/24"
+}
+variable "subnetid2" {
+  default = "10.0.2.0/24"
+}
+variable "keyname" {
+  default = "terrakey"
+}
+variable "rname" {
+  default = "us-west-2"
+}
+variable "amiid" {
+  default = "ami-03f65b8614a860c29" 
+}
+variable "instype" {
+  default = "t2.medium"
+}
+variable "vpcid" {
+  default = "10.0.0.0/16"
+}

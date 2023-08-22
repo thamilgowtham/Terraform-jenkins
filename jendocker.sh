ffe61da7c51b75589ac0aca22e76620381387ba8
thamilgowtham <135104448+thamilgowtham@users.noreply.github.com>
2023-08-22 13:35:06 +1000

    Add files via upload

---
+++ jendocker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install openjdk-17-jre -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install jenkins -y
+sudo apt update -y
+sudo curl -fsSL https://get.docker.com -o install-docker.sh
+sudo sh install-docker.sh -y 
+sudo service docker start
+sudo apt install maven -y

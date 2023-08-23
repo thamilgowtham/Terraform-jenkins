218c66376fd58d63c44242469d27015fcdbb89dc
thamilgowtham <135104448+thamilgowtham@users.noreply.github.com>
2023-08-23 11:52:23 +1000

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,26 +4,14 @@ variable "vmname" {
 variable "azone1" {
   default = "us-west-2a" 
 }
-variable "azone2" {
-  default = "us-west-2b"
-}
 variable "subname1" {
   default = "Sub01"
-}
-variable "subname2" {
-  default = "Sub02"
 }
 variable "Netif1" {
   default = "NIF01"
 }
-variable "Netif2" {
-  default = "NIF02"
-}
 variable "subnetid1" {
   default = "10.0.1.0/24"
-}
-variable "subnetid2" {
-  default = "10.0.2.0/24"
 }
 variable "keyname" {
   default = "terrakey"

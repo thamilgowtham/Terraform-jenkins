0183ec9ceb04af1b9566649931a87b80383369b5
thamilgowtham <135104448+thamilgowtham@users.noreply.github.com>
2023-08-24 21:12:15 +1000

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -10,3 +10,4 @@ sudo mv sonarqube-8.9.10.61524/ sonarqube
 sudo cp -r sonarqube /opt/
 sudo useradd -M -d /opt/sonarqube -s /bin/bash -r sonar
 sudo chown -R sonar:sonar /opt/sonarqube
+sudo hostnamectl set-hostname sonarqube

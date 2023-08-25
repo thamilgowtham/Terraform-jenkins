857e0b83024b6a9605186beee9c5bfbdc9aefe57
thamilgowtham <135104448+thamilgowtham@users.noreply.github.com>
2023-08-25 18:19:36 +1000

    Add files via upload

---
+++ kubenode.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo ufw disable
+sudo swapoff -a; sed -i '/swap/d' /etc/fstab
+sudo cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+sudo apt-get install     ca-certificates     curl     gnupg     lsb-release
+sudo apt-get update
+sudo apt install -y containerd
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+sudo echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+sudo apt update && apt install -y kubeadm kubelet kubectl
+modprobe br_netfilter
+cat >>/etc/sysctl.conf<<EOF
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl -p /etc/sysctl.conf
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sudo sysctl -p
+sudo hostnamectl set-hostname Knode

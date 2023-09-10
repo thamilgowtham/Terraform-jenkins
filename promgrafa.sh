46d42e5f0929d0b6211435809cbab0619a7cb16c
thamilgowtham <135104448+thamilgowtham@users.noreply.github.com>
2023-09-10 10:21:12 +1000

    Update promgrafa.sh

--- promgrafa.sh
+++ promgrafa.sh
@@ -47,7 +47,7 @@ sudo apt-get install -y software-properties-common wget
 sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get update -y
-sudo apt-get install grafana
+sudo apt-get install grafana -y
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
 sudo hostnamectl set-hostname promgrafa
